7dc4d99a4e51da916612dbe4c61ab0094529efcb
wqlab0000 <w.q.lab00@gmail.com>
2019-07-31 00:41:42 +0100

    Update insulinGlucose0112.m

--- src-12/insulinGlucose0112.m
+++ src-12/insulinGlucose0112.m
@@ -22,12 +22,8 @@ cp_array=[1 1 1 1 1 1 1 1 1 1];
 
 disp(' What would you like to explore ? ')
 
-disp(' 1. G <= 9 ' )
-disp(' 2. G >= 4.5')
-disp(' 3. I >= 0.04 ' )
-disp(' 4. I <= 0.2 ')
-disp(' 5. G >=4.5 /\ i>=0.04' )
-disp(' 6. G<=9 /\ i<=0.2')
+disp(' 1. scale=2, A = [-1 0 0], b = -4.5' )
+disp(' 2. scale=1, A = [-0.9 0 0], b = -4.5')
 
 
 
@@ -39,7 +35,7 @@ opt = input( 'Please select an option : ')
 disp('You selected')
 disp(opt)
 
-if (opt < 1 || opt > 6) 
+if (opt < 1 || opt > 2) 
     disp('Not a legal option!')
     return
 end
@@ -47,19 +43,8 @@ end
 
 switch opt
 
+        
 case 1
-        
-        scale = 1;
-        phi = '[] g_1';
-        
-        preds(1).str='g_1'; %G_1<=9
-        preds(1).A = [1 0 0 ];
-        preds(1).b = 9; 
-        propName='(G_1<=9) ';
-        fName='Data-01.txt';
-
-        
-case 2
         
         scale = 2;
         phi = '[] g_2';
@@ -69,63 +54,18 @@ case 2
         preds(1).b = -4.5; 
         propName='(G_2>=4.5)';
         fName='Data-02.txt';
-
-case 3
-        
-        scale = 50;
-        phi = '[] i_1';
-        
-        preds(1).str='i_1'; %I_1>=0.04
-        preds(1).A = [0 -1 0];
-        preds(1).b = -0.04; 
-        propName='(I_1>=0.04) ';
-        fName='Data-03.txt';
-        
-case 4
-        
-        scale = 100;
-        phi = '[] i_2';
-        
-        preds(1).str='i_2'; %I_2<=0.14
-        preds(1).A = [0 1 0];
-        preds(1).b =  0.14; 
-        propName=' (I_2<=0.14) ';
-        fName='Data-04.txt';
-
-
-        
-case 5
+case 2
         
         scale = 1;
-        phi = '[] i_1 /\ [] g_1';
+        phi = '[] g_2';
         
-        preds(1).str='g_1'; %G_1>=4.5
-        preds(1).A = [-1 0 0 ];
+        preds(1).str='g_2'; %G_1>=4.5
+        preds(1).A = [-0.9 0 0 ];
         preds(1).b = -4.5; 
-        
-        preds(2).str='i_1'; %I_1<=0.14
-        preds(2).A = [0 1 0];
-        preds(2).b =  0.14; 
-        propName=' ([] G >=4.5 /\ [] i<=0.14) ';
-        fName='Data-05.txt';
-        
-case 6
-        
-        scale = 1;
-        phi = '[] i_2 -> [] g_2';
-        
-        preds(1).str='g_2'; %G_1<=9
-        preds(1).A = [1 0 0];
-        preds(1).b = 9 ; 
-        
-        preds(2).str='i_2'; %I_1>=0.04
-        preds(2).A = [0 -1 0];
-        preds(2).b = -0.04; 
-        propName=' (G<=9 /\ i>=0.04) ';
-        fName='Data-04.txt';
-        
-        
-    
+        propName='(G_2>=4.5)';
+        fName='Data-02.txt';
+
+
 
 end
 
@@ -164,12 +104,9 @@ fid = fopen(fName,'a');
     title('Run #'+num2str(i));
     subplot(1,3,1);
     plot(T, Y(:,1));
-    subplot(1,3,2);
-    plot(T, 50 * Y(:,2));
-    subplot(1,3,3);
+    subplot(1,2,2);
     plot(T, Y(:,2));
-%     subplot(1,3,3);
-%     plot(T, Y(:,3));
+   
          
    
    fprintf (fid,' Best input for simulation run # %d\n',i);
