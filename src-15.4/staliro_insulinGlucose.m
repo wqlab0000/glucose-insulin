8f6dbc1894069fe8b983a01eadfdf711a85390de
wqlab0000 <w.q.lab00@gmail.com>
2019-09-16 09:24:24 +0100

    Update staliro_insulinGlucose.m

--- src-15.4/staliro_insulinGlucose.m
+++ src-15.4/staliro_insulinGlucose.m
@@ -1,5 +1,5 @@
 clear 
-mdl = 'insulinGlucose';
+mdl = 'insulinGlucoseSimHumanCtrl';
 cp_array=[1 1 1 1 1 1 1 1 1 1];
 
 init_cond = [];
@@ -9,7 +9,10 @@ type file_name.txt;
 x = input('\n Input a file name you want to explore \n','s')
 
 input_range = importdata(x); %import input_range data from text file
+%[m,n] = size(input_range);
 b = reshape(input_range, 10, 2);
+
+% disp(importdata(x))
 disp(b)
 
 
@@ -18,7 +21,13 @@ disp(' What would you like to explore ? ')
 disp(' 1. G >= 4.5 ' )
 disp(' 2. G <= 9 ' )
 disp(' 3. G >= 4.5 /\ G <= 9 ' )
-disp(' 4. 2G >= 9 /\ G <= 9 ' )
+disp(' 4. I<= 0.14 ' )
+disp(' 5. I <= 0.14 /\ G >= 4.5 ' )
+disp(' 6. I >= 0.04 /\ G <= 9 ' )
+disp(' 7. I >= 0.04 /\ G <= 9 ' )
+disp(' 8. I >= 0.04 /\ G <= 9 ' )
+
+
 
 disp(' Please select option: ' )
 choice = input( 'Please select an option : ')
@@ -26,7 +35,7 @@ choice = input( 'Please select an option : ')
 disp('You selected')
 disp(choice)
 
-if (choice < 1 || choice > 4) 
+if (choice < 1 || choice > 8) 
     disp('Not a legal option!')
     return
 end
@@ -42,7 +51,7 @@ switch choice
         preds(1).b = -4.5; 
         
         propName='phi = (G >= 4.5) ';
-        fName='15.4-case1.txt';
+        fName='case1.txt';
         
    case 2
         phi = '[] g_2';
@@ -52,7 +61,7 @@ switch choice
         preds(1).b = 9 ;
 
         propName='phi = (G <= 9 ) ';
-        fName='15.4-case2.txt';
+        fName='case2.txt';
 
    case 3
         phi = '[] g_1 /\ [] g_2';
@@ -64,20 +73,72 @@ switch choice
         preds(2).b = 9 ;
 
         propName='phi = (G >= 4.5 /\ G <= 9 ) ';
-        fName='15.4-case3.txt';
+        fName='case3.txt';
+ 
+   case 4 
+        phi = '[] i_1';
+
+        preds(1).str='i_1';  % I <= 0.14
+        preds(1).A = [0 1 0];
+        preds(1).b = 0.14 ; 
+                  
+        propName='phi = (I<= 0.14) ';
+        fName='case4.txt';
         
-   case 4
-        phi = '[] g_1 /\ [] g_2';
-        preds(1).str='g_1'; % 2G >= 9
-        preds(1).A = [-2 0 0];
-        preds(1).b = -9 ; 
+   case 5
+        phi = '[] i_2 /\ []_[100,400] g_1';
+        preds(1).str='i_2'; % i <= 0.14
+        preds(1).A = [0 1 0];
+        preds(1).b = 0.14 ; 
+       
+        preds(2).str ='g_1'; % G>=4.5
+        preds(2).A = [-1 0 0 ];
+        preds(2).b =  -4.5 ;
+
+        propName='phi = (I <=0.14 /\ G>=4.5) ';
+        fName='case5.txt';
+        
+        
+   case 6
+        phi = '[] i_2 /\ [] g_2';
+        preds(1).str='i_2'; % i >= 0.04
+        preds(1).A = [0 -1 0];
+        preds(1).b = -0.04 ; 
+       
         preds(2).str ='g_2'; % G<=9
         preds(2).A = [1 0 0 ];
         preds(2).b = 9 ;
 
-        propName='phi = (2G >= 9 /\ G <= 9 ) ';
-        fName='15.4-case4.txt';
-        
+        propName='phi = (I >=0.04 /\ G <= 9 ) ';
+        fName='case6.txt';
+    
+   case 7
+          phi = '[] i_1 /\ [] g_2';
+
+          preds(1).str='i_1';  % I <= 0.14
+          preds(1).A = [0 1 0];
+          preds(1).b = 0.14 ;
+
+          preds(2).str ='g_2'; % G <=9
+          preds(2).A = [1 0 0 ];
+          preds(2).b = 9 ;
+
+          propName='phi = (I<= 0.14 /\ G >=4.5 ) ';
+          fName='case7.txt';
+
+    case 8
+          phi = '[] i_2 /\ []_[100 400] g_1';
+
+          preds(1).str='i_2';  % I >= 0.04
+          preds(1).A = [0 -1 0];
+          preds(1).b = -0.04 ;
+
+          preds(2).str ='g_1'; % G >=4.5
+          preds(2).A = [-1 0 0 ];
+          preds(2).b = -4.5 ;
+
+          propName='phi = (I >= 0.04 /\ G >=4.5 ) ';
+          fName='case8.txt';    
         
  
 end
@@ -102,48 +163,113 @@ opt.interpolationtype={'const'};
 opt;
 
 opt.optimization_solver = 'SA_Taliro';
-
 opt.optim_params.n_tests=10; %The total number of tests to be executed
 % opt.taliro = 'dp_taliro';
 
 
+% fid = fopen(fName,'a');
 
-mkdir 15.4 % create a result folder and save output file in result folder
-savePath = '/path/to/15.4/'; 
+mkdir 1 % create a result folder and save output file in result folder
+savePath = '/Users/quanw/Desktop/trunk/benchmarks/insulinGlucoseHumanCtrlRisk/hovorkaModel/insulinGlucose2/1/'; 
 fid = fopen([savePath fName],'at'); 
 result =[fName '\t'];
 
 fprintf (fid,'------------------------------------------------------------------------------------\n');
 fprintf (fid, 'file name: %s \n', fName);
+% fprintf (fid, '\n starting opt runs\n');
 
     
     for i = 1:opt.runs
      [results, history] = staliro(mdl, init_cond, input_range, cp_array, phi, preds,time,opt);
      [T,~,Y,IT] = SimSimulinkMdl(mdl,init_cond,input_range,cp_array,results.run(results.optRobIndex).bestSample(:,1),time,opt);
 
-   
-   
     rob = results.run(results.optRobIndex).bestRob; % robustness value
-    
-    [min_Y, index]= min(Y,[],1) %find the lowest glucose level
-    minG = min_Y(1,1)
-    minIndex = index(1,1) % lowest glucose corresponding index
-    minT = T(minIndex,1)  % lowest glucose corresponding time
-        
-    
-   if preds(1).str == 'g_1' 
-    if minG < 4.5 && minG >= 2.5 && maxG > 9 && 2*(4.5-minG)>(maxG-9)  % lowest glucose level is [2.5 4.5]--dangerous     
-       rob = rob * 2; % we scale the robustness to twice larger than the original robustness
-    elseif minG < 4.5 && minG >= 2.5 && maxG <= 9 
-       rob = rob * 2; 
-   elseif minG < 2.5  % glucose level is within [0, 2.5)-- extremely dangerous
-       rob = -9999;    % robustness is set to negative infinity, here we set to -9999                   
-   else rob = rob;     % otherwise robustness keep the original value
-    end
-   end
 
- 
-     
+   [max_Y, index]= max(Y,[],1) 
+    maxG = max_Y(1,1)     %find the highest glucose level
+    maxIndex = index(1,1) % highest glucose corresponding index
+    maxT = T(maxIndex,1)  % highest glucose corresponding time
+    maxI =max_Y(1,2)      %find the highest insulin dose
+
+   [min_Y, index]= min(Y,[],1) 
+    minG = min_Y(1,1)      %find the lowest glucose level
+    minIndex = index(1,1)  % lowest glucose corresponding index
+    minT = T(minIndex,1)   % lowest glucose corresponding time
+    minI =min_Y(1,2)      %find the lowest insulin dose
+
+
+    
+%     if preds(1).str == 'g_1' 
+%         if minG < 4.5 && minG >=2.5 && maxG>9 && 2*(4.5-minG)>(maxG-9)  % lowest glucose level is [2.5 4.5]--dangerous     
+%        rob = rob * 2; % we scale the robustness to twice larger than the original robustness
+%         elseif minG<4.5 && maxG <=9 
+%        rob = rob * 2; 
+%         elseif minG < 2.5  % glucose level is within [0, 2.5)-- extremely dangerous
+%        rob = rob * 100;    % robustness is set to 100*rob   
+%     else rob = rob;     % otherwise robustness keep the original value
+%         end
+%     end
+  
+if preds(1).str == 'g_1'
+  if minG < 4.5 && ((maxG>9 && 2*(4.5-minG)>(maxG-9))|| maxG <=9 )
+     rob = -(10*(minG-4.5))^2;
+% we scale the robustness when glucose level under 4.5
+else rob = rob;     % otherwise robustness keep the original value
+  end
+end
+
+
+%     if preds(1).str == 'i_1'  
+%       if maxI>0.14            % insulin dose is over 0.14 
+%       rob = rob*1000;          % robustness is set to 1000*rob
+%        elseif minG<4.5  
+%        rob = rob * 2; 
+%         elseif minG < 2.5  % glucose level is within [0, 2.5)-- extremely dangerous
+%        rob = rob * 100;    % robustness is set to 100*rob   
+%       else rob = rob;
+%       end
+%     end
+    
+%     if preds(1).str == 'i_2'  
+%       if minI<0.04 && minG>=4.5   % insulin dose is under 0.04
+%       rob = rob*1000;          % robustness is set to 1000*rob
+%       else rob = rob;
+%       end
+%     end
+    
+ if preds(1).str == 'i_1'
+    if maxI>0.14             % insulin dose is over 0.14
+    rob = rob*1000;          % robustness is set to 1000*rob
+    else rob = rob;
+    end
+  end
+
+
+  if preds(1).str == 'i_2'
+    if maxI>0.14 && minG>=4.5 % insulin dose is over 0.14
+    rob = rob*1000;           % robustness is set to 1000*rob
+    elseif minG<4.5
+    rob = rob*10
+    elseif minG<2.5
+    rob = rob * 100;
+    else rob = rob;
+    end
+  end
+    
+    
+%      if  x == lm_input
+%      if rob >=0   % if robustness is zero or a positive value, glucose level is equal or over 4.5.                   
+%         rob = rob;   % robustness keep the original positive value.  
+%      elseif rob < 0 && rob >= -2 % if robustness is less than zero but over -2, glucose level is within [2.5 4.5] --dangerous  
+%         rob = 2 * rob; % we scale the robustness to twice larger than the original robustness.     
+%      else rob = -9999;    %if robustness is less than 2, glucose level is within [0, 2.5)-- extremely dangerous
+%                          % robustness is set to negative infinity, here we set to -9999                   
+%      end  
+%      end
+
+   
+   
+  
      
    fprintf (fid, '\n Input file name: %s \n', x);
    fprintf (fid, '%f %f \n', b(1, :)); 
@@ -165,6 +291,10 @@ fprintf (fid, 'file name: %s \n', fName);
    
    fprintf (fid,'\n Meal CHO planned: %f, actual: %f \n', IT(1,4), IT(1,9));
    fprintf (fid,'\n Lowest Glucose level: %f, Time: %f \n', minG, minT); 
+   fprintf (fid,'\n Highest Glucose level: %f, Time: %f \n', maxG, maxT); 
+   fprintf (fid,'\n Highest Insulin dose: %f \n', maxI); 
+
+
 
    fprintf (fid, ' \n Scaled-Robustness: %f', rob);
    fprintf (fid, ' \n Robustness: %f, Runtime: %f seconds\n', results.run(results.optRobIndex).bestRob,results.run(results.optRobIndex).time);
@@ -178,8 +308,11 @@ fprintf (fid, 'file name: %s \n', fName);
    
    
     figure ;
+%     title('Run #'+num2str(i));
     subplot(1,2,1);
     plot(T, Y(:,1));
+    subplot(1,2,2);
+    plot(T, Y(:,2));
 
 
 
